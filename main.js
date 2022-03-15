99cc7a409333e7a9a4cf8e9e535402c24a9a8a82
marselinusharson <marselinus@undiksha.ac.id>
2022-03-15 10:24:29 +0800

    membuat responsive list kategory screen mobile

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const modeToggle = document.querySelector(".dark-light");
 const searchToggle = document.querySelector(".searchToggle");
 const sidebarClose = document.querySelector(".sidebarClose");
 const sidebarOpen = document.querySelector(".sidebarOpen");
+const navLinks = document.getElementById("nav-links");
+const kategory = document.createElement("li");
+const text = document.createElement("a");
 
 //mode gelap dan mode terang
 modeToggle.addEventListener("click", () => {
@@ -18,10 +21,22 @@ searchToggle.addEventListener("click", () => {
 //sidebar modeToggle
 sidebarOpen.addEventListener("click", () => {
   nav.classList.add("active");
+  tambahList();
 });
 sidebarClose.addEventListener("click", (e) => {
   let clickedElm = e.target;
   if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
     nav.classList.remove("active");
+    hapusList();
   }
 });
+
+function tambahList() {
+  text.textContent = "Category";
+  kategory.appendChild(text);
+  navLinks.appendChild(kategory);
+}
+function hapusList() {
+  kategory.removeChild(kategory.lastChild);
+  navLinks.removeChild(navLinks.lastChild);
+}
